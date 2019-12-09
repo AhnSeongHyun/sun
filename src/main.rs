e58aeda245f61c080476e76d3f9d392217bc9a76
AhnSeongHyun <sh84.ahn@gmail.com>
2019-12-09 18:36:21 +0900

    Add version, help

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("sun")
+        .version("0.0.1")
+        .about("Utils for me")
+        .author("ash84")
+        .get_matches();
 }
