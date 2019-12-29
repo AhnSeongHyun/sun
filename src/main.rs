08d0f35c8fb906d41c28987df003026baa526daf
AhnSeongHyun <sh84.ahn@gmail.com>
2019-12-30 02:40:34 +0900

    Apply clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,38 @@
+mod config;
 mod misc;
+
+extern crate clap;
+use clap::{App, Arg, ArgMatches};
+use std::option::Option;
 use std::string::String;
+type HostFilePath = String;
+type Input = String;
 
 fn main() {
-    let file_path: String = misc::convert_resource_path(String::from("host.yaml"));
-    let content = misc::load_yaml(file_path);
-    println!("{}", content); 
+    let matches = App::new("sun")
+        .version("1.0")
+        .author("ash84 <me@ash84.io>")
+        .about("Run shell command to multiple hosts")
+        .args_from_usage(
+            "-c, --config=[FILE] 'Sets a config.yaml file'
+            <INPUT>            'Sets the input file to use'
+            ",
+        )
+        .get_matches();
+
+    let (host_file_path, input_command) = parse_arg(matches);
+    let config = config::Config {
+        host_file_path: host_file_path,
+    };
+
+    // let file_path: String = misc::convert_resource_path(String::from("host.yaml"));
+    // let content = misc::load_yaml(file_path);
+    println!("{:#?}", input_command);
+    println!("{:#?}", config);
+}
+
+fn parse_arg(matches: ArgMatches) -> (HostFilePath, Input) {
+    let host_file_path = matches.value_of("config").unwrap_or("");
+    let input_command = matches.value_of("INPUT").unwrap_or("");
+    return (host_file_path.to_string(), input_command.to_string());
 }
