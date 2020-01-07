d29321a1801d5c48e686e85340510938fc99e326
ash84 <ash84@rainist.com>
2020-01-07 17:48:38 +0900

    Add test_arg

---
+++ tests/test_arg.rs
@@ -0,0 +1,18 @@
+#[cfg(test)]
+mod test {
+    use clap::ArgMatches;
+    use std::collections::HashMap;
+    use std::string::String;
+    use sun::arg::parse_arg;
+
+    #[test]
+    fn test_parse_arg_empty_case() {
+        let mut args = HashMap::new();
+        let empty_matches = ArgMatches {
+            args: args,
+            subcommand: None,
+            usage: None,
+        };
+        assert_eq!(parse_arg(matches), (String::from(""), String::from("")));
+    }
+}
