eee58c7c921b6210ef523b30ee55b08621e03a42
ash84 <ash84@rainist.com>
2020-01-10 20:38:28 +0900

    Add test_parse_arg

--- tests/test_arg.rs
+++ tests/test_arg.rs
@@ -15,4 +15,16 @@ mod test {
         };
         assert_eq!(parse_arg(matches), (String::from(""), String::from("")));
     }
+
+    #[test]
+    fn test_parse_arg() {
+        let mut args = HashMap::new();
+        let empty_matches = ArgMatches {
+            args: args,
+            subcommand: None,
+            usage: None,
+        };
+        assert_eq!(parse_arg(matches), (String::from(""), String::from("")));
+    }
+
 }
