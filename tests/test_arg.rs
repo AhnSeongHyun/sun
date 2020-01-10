8cd5c88838fdea20a148af8a226aab33b1aeb123
ash84 <ash84@rainist.com>
2020-01-10 21:23:02 +0900

    Add todo for testing

--- tests/test_arg.rs
+++ tests/test_arg.rs
@@ -1,30 +1,19 @@
 #[cfg(test)]
 mod test {
-    use clap::ArgMatches;
+    use clap::{App, Arg, ArgMatches, Values};
     use std::collections::HashMap;
     use std::string::String;
     use sun::arg::parse_arg;
 
     #[test]
     fn test_parse_arg_empty_case() {
-        let mut args = HashMap::new();
-        let empty_matches = ArgMatches {
-            args: args,
-            subcommand: None,
-            usage: None,
-        };
+        let matches = App::new("empty test").get_matches();
         assert_eq!(parse_arg(matches), (String::from(""), String::from("")));
     }
 
     #[test]
     fn test_parse_arg() {
-        let mut args = HashMap::new();
-        let empty_matches = ArgMatches {
-            args: args,
-            subcommand: None,
-            usage: None,
-        };
-        assert_eq!(parse_arg(matches), (String::from(""), String::from("")));
+        // TODO : https://github.com/clap-rs/clap/issues/1616
     }
 
 }
